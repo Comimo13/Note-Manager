d05e44e7c3dcaf091cc2881fd97c1d8df0e8b253
Afzal <arradarra07@gamil.com>
2026-05-10 10:11:54 +0500

    new cli

--- commands/search.py
+++ commands/search.py
@@ -3,7 +3,7 @@ def search():
     try:
         with open('D:/Projects/Notes Manager/storage/notes.json' , 'r') as f:
             notes = json.load(f)
-        note = input()
+        note = input('PS C:\\WINDOWS\\System32> ')
         if note in notes:
             print(note)
         if note not in notes:
