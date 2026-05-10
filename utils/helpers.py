6588e34b6808b563b3ac00b8d674627bdc72e5a9
Afzal <arradarra07@gamil.com>
2026-05-10 10:12:04 +0500

    helper function

---
+++ utils/helpers.py
@@ -0,0 +1,6 @@
+def helper():
+    print('every command starts with "nm"')
+    print('if you want to create a new note , write create')
+    print('if you want to delete a note , write delete')
+    print('if you want to see all notes , write list-notes')
+    print('if you want to search a note , write search')
